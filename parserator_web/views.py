d1a8791f02ba441478858e22bb0ab7f6c6b28a77
Noah Appelbaum <noahappelbaum@gmail.com>
2024-07-11 22:32:53 -0500

    implemented AddressParse APIView get endpoint and tests

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -1,9 +1,11 @@
 import usaddress
+from usaddress import RepeatedLabelError
 from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.exceptions import ParseError
+from rest_framework import status
 
 
 class Home(TemplateView):
@@ -14,11 +16,44 @@ class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        # TODO: Flesh out this method to parse an address string using the
-        # parse() method and return the parsed components to the frontend.
-        return Response({})
+        """
+        API endpoint to parse a U.S. address
+        expects a string "address" query
+        responds with JSON, like:
+            {
+                "input_string": "123 main street"
+                "address_components": {"AddressNumber": "123", ...},
+                "addres_type": "Street Address"
+            }
+        """
+        form_data = request.query_params
+        if ("address" not in form_data):
+            raise ParseError
+
+        input_string = form_data["address"]
+
+        try:
+            (address_components, address_type) = self.parse(input_string)
+            response_object = {}
+            response_object["input_string"] = input_string
+            response_object["address_components"] = address_components
+            response_object["address_type"] = address_type
+
+            return Response(response_object)
+
+        except RepeatedLabelError:
+            error_message = "Repeated Label Error; check that address is correct"
+            return Response(
+                {"error": error_message}, status=status.HTTP_400_BAD_REQUEST
+            )
+        # TODO: Add except clauses for any other errors raised by usaddress
 
     def parse(self, address):
-        # TODO: Implement this method to return the parsed components of a
-        # given address using usaddress: https://github.com/datamade/usaddress
+        """
+        Parses a United States address string into its component parts,
+        using usaddress: https://github.com/datamade/usaddress.
+
+        -> address_components: OrderedDict, address_type: str
+        """
+        (address_components, address_type) = usaddress.tag(address)
         return address_components, address_type
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,41 @@
 import pytest
+import json
+
+parse_url = "/api/parse/"
 
 
 def test_api_parse_succeeds(client):
-    # TODO: Finish this test. Send a request to the API and confirm that the
-    # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    response = client.get(parse_url, {"address": address_string})
 
+    assert response.status_code == 200
+
+    parsed_data = json.loads(response.content)
+    assert parsed_data == {
+        "input_string": "123 main st chicago il",
+        "address_components": {
+            "AddressNumber": "123",
+            "StreetName": "main",
+            "StreetNamePostType": "st",
+            "PlaceName": "chicago",
+            "StateName": "il"
+        },
+        "address_type": "Street Address"
+    }
 
 def test_api_parse_raises_error(client):
-    # TODO: Finish this test. The address_string below will raise a
-    # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    response = client.get(parse_url, {"address": address_string})
+
+    assert response.status_code == 400
+
+def test_api_parse_raises_error_on_missing_address_string(client):
+    response = client.get(parse_url, {})
+
+    assert response.status_code == 400
+
+def test_api_parse_succeeds_on_empty_string(client):
+    address_string = ""
+    response = client.get(parse_url, {"address": address_string})
+
+    assert response.status_code == 200
